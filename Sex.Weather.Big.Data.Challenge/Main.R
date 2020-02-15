0f5b9787260b4474bf2a7c4c9f1b2297770c4254
kjdurovich <keegan.durovich@questu.ca>
2020-02-14 17:27:01 -0800

    Main r file created

---
+++ Sex.Weather.Big.Data.Challenge/Main.R
@@ -0,0 +1,36 @@
+#label working directory
+wd <- getwd()
+
+
+
+
+#====INDEX======================================================================
+
+
+
+#====Libraries==================================================================
+#install libraries
+
+
+
+
+# names of folders for output data (figures,data output,data)
+# store names of the folders in an object
+output.folder.names <- c("Figures", "Code", "Data")
+# and make the folders if they don't exit yet. No need to understand this now
+for(i in 1:length(output.folder.names)) 
+  if(file.exists(output.folder.names[i]) == FALSE) 
+    dir.create(output.folder.names[i])
+
+# Create Folder Paths
+#Path to figures folder
+path.figures <- paste(wd, "/", output.folder.names[1], "/", sep = "")
+#Path to code folder
+path.data.output <- paste(wd, "/", output.folder.names[2], "/", 
+                          sep = "")
+#Path to data folder
+path.data <- paste(wd, "/", output.folder.names[3], "/", sep = "")
+
+#Example of posting to the datafolder
+write.csv(output.folder.names, paste(path.data.output, "uselessnames.csv"),
+          row.names = FALSE)
