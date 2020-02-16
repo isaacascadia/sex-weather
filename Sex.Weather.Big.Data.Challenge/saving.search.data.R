b7705b65abf54910e691cae05cfeafe815c46f4a
isaacascadia <isaac.roter@questu.ca>
2020-02-15 19:05:16 -0800

    gtrend search > 6+6 month edition (try without widget error!)

--- Sex.Weather.Big.Data.Challenge/saving.search.data.R
+++ Sex.Weather.Big.Data.Challenge/saving.search.data.R
@@ -15,33 +15,54 @@
 # files for each query
 
 
+
+
 # loop to create .csv for first half of gtrends queries
 for(i in 1:(length(big.cities$name)/2)){
   
-  # saving gtrends query to object
-  list <- gtrendsR::gtrends(geo = as.character(big.cities[i,3]), 
-                  category = 1236, onlyInterest = TRUE)
-   
-   # saving object to .csv
-   write.csv(list$interest_over_time, paste(wd, "/data.output/", 
-                                            as.character(big.cities[i,1]), 
-                                            ".gtrends", ".csv", sep = ""))
-  }  # end of loop
-
+  # querying gtrends for first half of year and saving to object
+  first.6mo <- gtrends(geo = as.character(big.cities[i,3]), category = 1236,
+                       time = "2018-01-01 2018-06-30", onlyInterest = TRUE)
+  
+  # querying gtrends for second half of year and saving to object
+  second.6mo <- gtrends(geo = as.character(big.cities[i,3]), category = 1236,
+                        time = "2018-07-01 2018-12-31", onlyInterest = TRUE)
+  
+  # combining first and second half to get whole year interest over time 
+  iot <- rbind(first.6mo$interest_over_time, second.6mo$interest_over_time)
+  
+# saving combined interest over time to .csv
+  write.csv(iot, paste(wd, "/data.output/", as.character(big.cities[i,1]), 
+                       ".gtrends", ".csv", sep = ""))
+}  # end of loop
 
 
 # loop to create .csv for second half of gtrends queries
-for(i in length(big.cities$name)/2+1:length(big.cities$name)){
+for(i in (length(big.cities$name)/2):length(big.cities$name)){
   
-  # saving gtrends query to object
-  list <- gtrendsR::gtrends(geo = as.character(big.cities[i,3]), 
-                            category = 1236, onlyInterest = TRUE)
+# querying gtrends for first half of year and saving to object
+  first.6mo <- gtrends(geo = as.character(big.cities[i,3]), category = 1236,
+                   time = "2018-01-01 2018-06-30", onlyInterest = TRUE)
+   
+# querying gtrends for second half of year and saving to object
+  second.6mo <- gtrends(geo = as.character(big.cities[i,3]), category = 1236,
+                   time = "2018-07-01 2018-12-31", onlyInterest = TRUE)
   
-  # saving object to .csv
-  write.csv(list$interest_over_time, paste(wd, "/data.output/", 
-                                           as.character(big.cities[i,1]), 
-                                           ".gtrends", ".csv", sep = ""))
+# combining first and second half to get whole year interest over time 
+  iot <- rbind(first.6mo$interest_over_time, second.6mo$interest_over_time)
+  
+# saving combined interest over time to .csv
+  write.csv(iot, paste(wd, "/data.output/", as.character(big.cities[i,1]), 
+                       ".gtrends", ".csv", sep = ""))
 }  # end of loop
+
+
+
+
+
+
+
+
 
 
 
@@ -53,8 +74,8 @@ csv.check <- rep(NA, 178)             # vector contains Boolean .csv presence
 for(i in 1:length(big.cities$name)){  # loop checks .csv presence for each city
 # populating vector w/ Boolean presence
   csv.check[i] <- file.exists(paste(wd, "/data.output/",  
-                                  as.character(big.cities[i,1]), ".csv",
-                                  ".gtrends", sep = ""))
+                                  as.character(big.cities[i,1]), ".gtrends", 
+                                  ".csv", sep = ""))
 }
 
 # which cities don't have a gtrends query .csv?
